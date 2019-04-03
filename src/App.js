e078ccd3f522002b4bcaaf1bcf7eeba83cf0318c
teknoformula <seraficaj@gmail.com>
2019-04-03 15:21:49 -0700

    added DB

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <Router>
         <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <Link to={"/"} className="navbar-brand" href="#">AppMapper</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
