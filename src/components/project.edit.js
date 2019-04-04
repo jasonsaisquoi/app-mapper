70e52ee6753a954654df535a3543e72553f1d4e6
teknoformula <seraficaj@gmail.com>
2019-04-04 11:48:03 -0700

    update project functionality

--- src/components/project.edit.js
+++ src/components/project.edit.js
@@ -1,11 +1,85 @@
 //edit project
 
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class ProjectEdit extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.onChangeProjectName = this.onChangeProjectName.bind(this);
+    this.onChangeProjectSummary = this.onChangeProjectSummary.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      project_name: '',
+      project_summary: ''
+    }
+  }
+  
+  componentDidMount() {
+    axios.get('http://localhost:4000/project/edit/'+this.props.match.params.id)
+      .then(response => {
+        this.setState({
+          project_name: response.data.project_name,
+          project_summary: response.data.project_summary
+        })
+      })
+      .catch( (err) => console.log(err))
+  }
+  onChangeProjectName(e) {
+    this.setState({
+      project_name: e.target.value
+    });
+  }
+  onChangeProjectSummary(e) {
+    this.setState({
+      project_summary: e.target.value
+    })  
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const obj = {
+      project_name: this.state.project_name,
+      project_summary: this.state.project_summary
+    };
+    axios.post('http://localhost:4000/project/update/'+this.props.match.params.id, obj)
+        .then(res => console.log(res.data));
+    
+    this.props.history.push('/index');
+  }
+
   render() {
     return (
-      <div>ProjectEdit Component</div>
+      <div className="container" style={{marginTop: 10+"px"}}>
+        <h2>Update Project</h2>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+              <label>Project Name:  </label>
+              <input 
+                type="text" 
+                className="form-control" 
+                value={this.state.project_name}
+                onChange={this.onChangeProjectName}
+                />
+          </div>
+          <div className="form-group">
+              <label>Project Summary:  </label>
+              <textarea
+                rows="5" 
+                className="form-control" 
+                value={this.state.project_summary}
+                onChange={this.onChangeProjectSummary}
+                />
+          </div>
+          <div className="form-group">
+            <input type="submit" 
+              value="Update Project" 
+              className="btn btn-primary"/>
+          </div>
+        </form>
+      </div>
     )
   }
 }
--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class ProjectCard extends Component {
   render() {
@@ -27,7 +28,7 @@ class ProjectCard extends Component {
                   <p className="card-text">{this.props.obj.project_summary} </p>
                   <p className="card-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                   <a href="#" className="btn btn-primary">Read More</a>
-                  <a href="#" className="btn btn-outline-success">Update</a>
+                  <Link to={`/edit/${this.props.obj._id}`} className="btn btn-outline-success">Update </Link>
                   <a href="#" className="btn btn-outline-danger">Delete</a>
                 </div>
               </div>
