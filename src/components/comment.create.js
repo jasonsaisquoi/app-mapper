bf868c5580b92240fa20dcddd9ad7cd688b79d53
teknoformula <seraficaj@gmail.com>
2019-04-06 10:32:28 -0700

    add submit button to comment form

--- src/components/comment.create.js
+++ src/components/comment.create.js
@@ -1,6 +1,13 @@
 //create.comment
+
 import React, { Component } from 'react';
+import axios from 'axios';
 export default class CommentCreate extends Component {
+
+  constructor(props){
+    super(props);
+  }
+
   render() { 
     return (
       <div className="container">
@@ -8,7 +15,10 @@ export default class CommentCreate extends Component {
         <form>
           <div className="form-group">
             <label>Throw sunshine, not shade!</label>
-            <input className="form-control" type="text"></input>
+            <textarea rows="5" className="form-control" type="text" />
+          </div>
+          <div className="form-group">
+            <input type="submit" className="btn btn-lg btn-outline-primary" value="Add Comment"/>
           </div>
         </form>
       </div>
